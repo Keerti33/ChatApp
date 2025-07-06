cc847dbd5d0c75e8398c1153e5709ff318191222
Keerti33 <golakomal20@gmail.com>
2025-07-07 01:21:03 +0530

    js

--- frontend/script.js
+++ frontend/script.js
@@ -5,10 +5,27 @@ const modelSelect = document.querySelector('model-select');
 
 let messages = [];
 
-sendBtn.addEventListener('click' , () => {
+sendBtn.onclick = async () => {
 const content = userInput.value;
 if(!content) return;
 messages.push({role: 'Keerti' , content});
 renderMessage();//To show on screen
 
+const res = await fetch('http://localhost:3000/api/chat', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ model: modelSelect.value, messages })
 });
+const data = await res.json();
+ const reply = data.choices?.[0]?.message?.content || 'No response';
+  messages.push({ role: 'assistant', content: reply });
+  renderMessages(); // show AI reply
+  userInput.value = ''
+
+
+};
+function renderMessages() {
+  chatBox.innerHTML = messages.map(m =>
+    `<div class="${m.role}">${m.role === 'user' ? '🧑' : '🤖'} ${m.content}</div>`
+  ).join('');
+}
